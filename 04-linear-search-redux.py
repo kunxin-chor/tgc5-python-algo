f0d3cabace90a32cb706e1e184dc2bc38e97a2de
kunxin-chor <chorkunxin@yahoo.com>
2020-04-08 09:17:44 +0000

    Added linear search qns

---
+++ 04-linear-search-redux.py
@@ -0,0 +1,11 @@
+import random
+
+"""
+Given a a list of random numbers between 0 to 20, count how many even numbers there are, and print out
+"""
+
+numbers = []
+for i in range(20):
+    numbers.append(random.randint(0,1000000))
+
+#### After line 11 ####
